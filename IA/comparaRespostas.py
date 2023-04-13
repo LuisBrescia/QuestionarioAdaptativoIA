c1bfbe448b08b8780472e38e042232b94e0c9f1a
Luis Brescia <luisfelipebrescia@gmail.com>
2023-04-13 09:11:01 -0300

    simplificando o código

--- IA/comparaRespostas.py
+++ IA/comparaRespostas.py
@@ -1,4 +1,5 @@
 import torch
+from transformers import BertTokenizer, BertModel
 
 # ? Modelo IA pré-treinado: BERT
 from transformers import AutoTokenizer, AutoModel
@@ -11,26 +12,31 @@ respostaIA = "Segunda resposta"
 
 # * Transforma as respostas em algo que o modelo entenda
 # // resposta1 = tokenizer.encode(resposta1, return_tensors='pt')    # tensor
-tokenAluno = tokenizer.encode(respostaAluno, add_special_tokens=False)
-tokenIA = tokenizer.encode(respostaIA, add_special_tokens=False)
+tokenAluno = tokenizer.encode(respostaAluno, add_special_tokens=True)
+tokenIA = tokenizer.encode(respostaIA, add_special_tokens=True)
+# > add_special_tokens=True: Adiciona os tokens Bert [CLS] e [SEP] para indicar o início e o fim da sentença.
 
-# * Adiciona os tokens especiais
-# ? Tokens Especiais são usados em modelos de linguagem natural para indicar o início e o fim de uma sentença.
-tokenAluno = [tokenizer.cls_token_id] + tokenAluno + [tokenizer.sep_token_id]
-tokensIA = [tokenizer.cls_token_id] + tokenIA + [tokenizer.sep_token_id]
+# ! Não precisa mais
+# tokenAluno = [tokenizer.cls_token_id] + tokenAluno + [tokenizer.sep_token_id]
+# tokensIA = [tokenizer.cls_token_id] + tokenIA + [tokenizer.sep_token_id]
 
 # * Transforma os tokens em tensores PyTorch
-inputs1 = torch.tensor([tokens1])
-inputs2 = torch.tensor([tokens2])
+# ? Tensores são estruturas de dados multidimensionais que podem ser usadas para representar vetores e matrizes.
+inputs1 = torch.tensor([tokenAluno])
+inputs2 = torch.tensor([tokenIA])
 
 # * Obter as saídas do modelo para cada sequência
 outputs1 = model(inputs1)
 outputs2 = model(inputs2)
 
 # * Extrair o último estado escondido de cada token
+# ? O último estado escondido é o vetor de características que representa o token.
 last_hidden_states1 = outputs1[0][:, 0, :]
 last_hidden_states2 = outputs2[0][:, 0, :]
 
 # * Calcular a similaridade entre as duas sequências
 from torch.nn.functional import cosine_similarity
 similarity = cosine_similarity(last_hidden_states1, last_hidden_states2)
+
+# * Imprimir o resultado
+print(f"A similaridade entre as duas respostas é: {similarity.item():.2f}")

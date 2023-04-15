a596bec8b8cecbd0a4dbc504008fcf0d2ddd73ce
Luis Brescia <luisfelipebrescia@gmail.com>
2023-04-15 17:36:46 -0300

    Backend e frontend conectados

--- IA/comparaRespostas.py
+++ IA/comparaRespostas.py
@@ -1,45 +1,45 @@
 from transformers import BertTokenizer, BertModel
-from flask import Flask, request
+from flask import Flask, render_template, request
 import torch
+import os
 
 # Nome do meu site
-questionario = Flask(__name__)
+questionario = Flask(__name__, template_folder='../FrontEnd')
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
 
 # Rota para a página inicial
-@questionario.route('/', methods=['POST'])
+@questionario.route("/")
 
 def comparaRespostas():
-    respostaAluno = request.form['respostaAluno']
-    with open('Respostas/resposta1.txt', 'r') as resposta1:
-        respostaIA = resposta1.read()
 
-    tokenAluno = tokenizer.encode(respostaAluno, add_special_tokens=True)
-    tokenIA = tokenizer.encode(respostaIA, add_special_tokens=True)
+    # # respostaAluno = request.form['respostaAluno']
+    # respostaAluno = "Primeira resposta"
+    # with open('Respostas/resposta1.txt', 'r') as resposta1:
+    #     respostaIA = resposta1.read()
 
-    inputs1 = torch.tensor([tokenAluno])
-    inputs2 = torch.tensor([tokenIA])
+    # tokenAluno = tokenizer.encode(respostaAluno, add_special_tokens=True)
+    # tokenIA = tokenizer.encode(respostaIA, add_special_tokens=True)
 
-    outputs1 = model(inputs1)
-    outputs2 = model(inputs2)
+    # inputs1 = torch.tensor([tokenAluno])
+    # inputs2 = torch.tensor([tokenIA])
 
-    last_hidden_states1 = outputs1[0][:, 0, :]
-    last_hidden_states2 = outputs2[0][:, 0, :]
+    # outputs1 = model(inputs1)
+    # outputs2 = model(inputs2)
 
-    from torch.nn.functional import cosine_similarity
-    similarity = cosine_similarity(last_hidden_states1, last_hidden_states2)
+    # last_hidden_states1 = outputs1[0][:, 0, :]
+    # last_hidden_states2 = outputs2[0][:, 0, :]
 
-    x = similarity.item() 
-    x = ((x + 1) / 2) * 100
-    print(f"A similaridade entre as duas respostas é de: {x:.2f}%")
+    # from torch.nn.functional import cosine_similarity
+    # similarity = cosine_similarity(last_hidden_states1, last_hidden_states2)
+    
+    # x = similarity.item() 
+    # x = ((x + 1) / 2) * 100
 
-    return f"A similaridade entre as duas respostas é de: {x:.2f}%"
+    # return f"A similaridade entre as duas respostas é de: {x:.2f}%"
+    return render_template('index.html')
 
+# Caso esteja rodando o arquivo diretamente, execute o servidor
 if __name__ == '__main__':
     questionario.run(debug=True)
-
-#respostaAluno = "Primeira resposta"
-#with open('Respostas/resposta1.txt', 'r') as resposta1:
-#    respostaIA = resposta1.read()
